855b15f26a3da0b78d29f457eff54700b5ea3c5d
Atsushi Handa <handa@pensees.co.jp>
2020-04-23 22:19:11 +0900

    clean: replaceUniqueStrのconfigの読み込みをclass依存に変更

--- modules/replaceUniqueStr.ts
+++ modules/replaceUniqueStr.ts
@@ -1,11 +1,13 @@
 {
-  const config = require('../config.json')
+  const CommonMethods = require('../class/CommonMethods.js')
+  const $ = new CommonMethods()
+  
   // ポータルIDとホストをターゲットポータルのものに書き換える
   module.exports = async function replaceUniqueStr(fn) {
     const all: { [key: string]: any }[] = await fn()
     const replacedStr = JSON.stringify(all)
-      .replace(new RegExp(config.origin.pid, 'g'), config.target.pid)
-      .replace(new RegExp(config.origin.host, 'g'), config.target.host)
+      .replace(new RegExp($.config.origin.pid, 'g'), $.config.target.pid)
+      .replace(new RegExp($.config.origin.host, 'g'), $.config.target.host)
     return JSON.parse(replacedStr)
   }
 }
