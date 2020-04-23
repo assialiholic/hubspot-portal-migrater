9ca0163cbb058bb765716587d38d0b6188319a03
Atsushi Handa <handa@pensees.co.jp>
2020-04-23 22:04:19 +0900

    clean: replaceUniqueStrのリファクタリング

--- modules/replaceUniqueStr.ts
+++ modules/replaceUniqueStr.ts
@@ -1,12 +1,11 @@
 {
   const config = require('../config.json')
   // ポータルIDとホストをターゲットポータルのものに書き換える
-  async function replaceUniqueStr(fn) {
+  module.exports = async function replaceUniqueStr(fn) {
     const all: { [key: string]: any }[] = await fn()
     const replacedStr = JSON.stringify(all)
       .replace(new RegExp(config.origin.pid, 'g'), config.target.pid)
       .replace(new RegExp(config.origin.host, 'g'), config.target.host)
     return JSON.parse(replacedStr)
   }
-  module.exports = replaceUniqueStr
 }
