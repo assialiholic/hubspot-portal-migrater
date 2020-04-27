b20fd3d3d9908d2a74d7759ad3ff9236a10285d7
Atsushi Handa <handa@pensees.co.jp>
2020-04-27 21:46:15 +0900

    add: deleteTargetPages.ts

---
+++ src/DANGEROUS/deleteTargetPages.ts
@@ -0,0 +1,26 @@
+{
+  const CommonMethods = require('./class/CommonMethods.js')
+  const $ = new CommonMethods()
+  const getAllPages = require('./modules/getAllPages.js')
+  const hs = new $.HubSpotClient({ hapikey: $.config.target.apiKey })
+
+  function deletePages(
+    pages: any[],
+    i: number
+  ): boolean | void {
+    if (i === pages.length) return false
+
+    hs.pages.deletePage(pages[i].id).then(res => console.log(res.name))
+
+    setTimeout(() => {
+      deletePages(pages, ++i)
+    }, 100)
+  }
+
+  getAllPages('target')
+    .then(targetPages => {
+      $.backup(targetPages, $.returnFileName(__filename))
+      deletePages(targetPages, 0)
+    })
+    .catch(err => console.error(err))
+}
