fa0ba6fb4d02d1e7eaed5ff20256acb45017338a
Atsushi Handa <handa@pensees.co.jp>
2020-04-28 21:58:31 +0900

    add: migrateTemplate.ts

---
+++ src/migrateTemplates.ts
@@ -0,0 +1,52 @@
+{
+  const CommonMethods = require('./class/CommonMethods.js')
+  const $ = new CommonMethods()
+  const replaceStr = require('./modules/replaceStr.js')
+  const getTemplates = require('./modules/getTemplates.js')
+  const returnFilePathReplaceArr = require('./modules/returnFilePathReplaceArr.js')
+
+  const createTempsUrl = new URL('https://api.hubapi.com/content/api/v2/templates')
+  createTempsUrl.searchParams.set('hapikey', $.config.target.apiKey)
+  function createTemps(temps: any[], i: number): boolean | void {
+    if (i === temps.length) return false
+
+    $.fetch(createTempsUrl.href, {
+      method: 'post',
+      body: JSON.stringify(temps[i]),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then(res => res.json())
+      .then(json => {
+        console.log(json.filename)
+      })
+      .catch(error => console.error(error))
+
+    setTimeout(() => {
+      createTemps(temps, ++i)
+    }, 100)
+  }
+
+  async function returnStrReplacedJson() {
+    const originTemps = await getTemplates('origin')
+    const filePathArr = await returnFilePathReplaceArr()
+    const replaceOption = filePathArr.concat([
+      {
+        before: $.config.origin.pid,
+        after: $.config.target.pid,
+      },
+      {
+        before: $.config.origin.host,
+        after: $.config.target.host,
+      },
+    ])
+    return replaceStr(originTemps, replaceOption)
+  }
+
+  returnStrReplacedJson()
+    .then(json => {
+      // createTemps(json, 0)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+}
--- src/modules/getTemplates.ts
+++ src/modules/getTemplates.ts
@@ -1,17 +1,19 @@
 {
   const CommonMethods = require('../class/CommonMethods.js')
   const $ = new CommonMethods()
-  const hsTarget = new $.HubSpotClient({ hapikey: $.config.target.apiKey })
 
   // 全テンプレートを配列として取得
-  module.exports = async function getTemplates() {
-    const res: { [key: string]: any } = await hsTarget.templates.getTemplates({
+  module.exports = async function getTemplates(portal: string) {
+    const apiKey =
+    portal === 'origin' ? $.config.origin.apiKey : $.config.target.apiKey
+  const hs = new $.HubSpotClient({ hapikey: apiKey })
+    const res: { [key: string]: any } = await hs.templates.getTemplates({
       limit: 1,
     })
     const total: number = res.total
     const allRes: {
       [key: string]: any
-    } = await hsTarget.templates.getTemplates({
+    } = await hs.templates.getTemplates({
       limit: total,
     })
     return allRes.objects
